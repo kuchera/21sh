370b8e372c135c46507a3c7383d5422b8146bf97
kuchera <link5525@hotmail.com>
2014-02-10 09:10:07 +0100

    Commentaires et fichiers pour CAT

---
+++ src/fun_cat.h
@@ -0,0 +1,12 @@
+#ifndef FUN_CAT_H
+#define FUN_CAT_H
+
+/*
+ * Fonction de lecture de fichiers
+ * cat FILENAME affiche le contenu
+ * du fichier FILENAME dans la
+ * console
+ */
+int fun_cat(int argc, char **argv);
+
+#endif
--- src/print.h
+++ src/print.h
@@ -4,7 +4,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// Affiche un message de bienvenue
 void p_welcome();
+// Affiche un message d'aide
 void p_help();
+// Affiche l'aide de la fonction id
+void p_man(char *id);
 
 #endif
