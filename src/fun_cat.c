dc9bf0b37e540eb6b3306a1426ba516f9cfdcaab
Nakos78 <yass.raz@hotmail.fr>
2014-02-16 18:36:03 +0000

    fonction cat fonctionnelle pour 1 ou plusieurs arguments.

--- src/fun_cat.c
+++ src/fun_cat.c
@@ -1,1 +1,37 @@
 #include "fun_cat.h"
+#include <stdio.h>
+
+
+int fun_cat (int argc, char *argv[])
+{
+	if (argc == 1) 
+    {
+        printf("Aucun argument...\n");
+        return 0;
+    }
+    
+    int nb_arg = argc-1;
+    while (nb_arg > 0)
+    {   
+        const char *filename =  argv[1];
+        FILE *file = fopen(filename,"r");
+
+        if (file != NULL)
+        {   
+            char line[128];
+            while (fgets(line, sizeof(line), file) != NULL)
+            {
+               printf("%s ", line);
+            }
+            fclose(file);
+        }
+        else
+        {
+          perror(filename);
+       }
+       --nb_arg;
+    }
+    return 0;
+}
+
+
--- src/main.h
+++ src/main.h
@@ -13,4 +13,4 @@
 
 int main();
 
-#endif 
+#endif
