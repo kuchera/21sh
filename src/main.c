178741e050e0a7dd3651bc7a95d29e5ace5c2e60
kuchera <link5525@hotmail.com>
2014-03-25 19:39:53 +0100

    Match quotes non teste

--- src/main.c
+++ src/main.c
@@ -1,10 +1,15 @@
 #include "main.h"
 
+#include "input.h"
+#include "my_tab.h"
+
 int main(int argc, char **argv)
 {
 	if (argc == 2 && !strcmp(argv[1], "-t"))
 	{
 		puts("=== TESTS ===");
+		my_tab t = read_command();
+		my_tprint(t);
 		return 0;
 	}
 	else
--- src/match.c
+++ src/match.c
@@ -2,10 +2,51 @@
 
 void match(my_tab argv)
 {
-	if (argv) {}
+	match_quotes(argv);
 }
 
 void match_quotes(my_tab tab)
 {
-	if (tab) {}
+	my_tab t = my_tnew();
+	int isq = 0;
+	char *s;
+	char *s2;
+	char *s3;
+	size_t l;
+	while (my_tlen(tab) > 0)
+		my_tadd(t, my_tpop(tab));
+	while (my_tlen(tab) > 0)
+	{
+		s = my_tpop(t);
+		l = strlen(s) - 1;
+		if (isq)
+		{
+			if (s[l] == '\"')
+			{
+				isq = 0;
+				s[l] = 0;
+			}
+			s2 = my_tpop(tab);
+			s3 = malloc(strlen(s) + strlen(s2) + 2);
+			sprintf(s3, "%s %s", s, s2);
+			free(s);
+			free(s2);
+			my_tadd(tab, s3);
+		}
+		else
+		{
+			if (*s == '\"')
+			{
+				isq = 1;
+				++s;
+				--l;
+				if (s[l] == '\"')
+				{
+					isq = 0;
+					s[l] = 0;
+				}
+			}
+			my_tadd(tab, s);
+		}
+	}
 }
